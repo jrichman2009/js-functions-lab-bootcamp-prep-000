211a7d3b79076ad2a642aac55ff8163fa185afba
Jeffrey Richman <jrichman2009@gmail.com>
2018-08-19 03:53:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code below!
+// write your code below //
+
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return 'Happy Holidays ${name}'
+}
+
+function happyHolidayTo(holiday, name) {
+  return 'Happy ${holiday}, ${name}'
+}
